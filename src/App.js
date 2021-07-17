fc184e80a55af8272dadd72b558d710ded57ba80
CedricAmbrosino <cedric.ambrosino.services@gmail.com>
2021-07-17 14:23:57 +0200

    version css react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Logo from './components/Logo';
+
+function App() {
+  return (
+    <div className="App">
+      <Logo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Logo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Logo.css';
+
+const Logo = () => {
+    return (
+        <div className='containerLogo'>
+            <div className="circle1"></div>
+            <div className="shape2">
+                <div className="circle"></div>
+                <div className="line"></div>
+            </div>
+            <div className="shape3">
+                <div className="circle"></div>
+                <div className="line"></div>
+            </div>
+            <div className="shape4">
+                <div className="circle"></div>
+                <div className="line"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Logo;
